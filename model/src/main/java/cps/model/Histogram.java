d525c32a37a1c864beb67a72ebe3236ba961f411
Jakub Mroczek <jakub.mroczek@tomtom.com>
2019-03-17 23:06:24 +0100

    first version of histogram model

--- model/src/main/java/cps/model/Histogram.java
+++ model/src/main/java/cps/model/Histogram.java
@@ -1,5 +1,50 @@
 package cps.model;
 
-public class Histogram {
+import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.DoubleStream;
+
+import static java.lang.Math.*;
+
+@Data
+public class Histogram {
+    // Liczba przedziałów
+    private final int bins;
+    private final double min, max;
+    private final List<Double> frequencyList;
+
+    public Histogram(SignalChart chart, int bins) {
+        this.bins = bins;
+
+        //TODO: Efficiency lost, two loops, maybe chart should know it min and max value?
+        //TODO: Change name from probes to samples
+        List<Double> samples = chart.getProbes();
+        min = Collections.min(samples);
+        max = Collections.max(samples);
+
+        frequencyList = countFrequencies(samples);
+    }
+
+    private List<Double> countFrequencies(List<Double> samples) {
+        final double columnLength = (max - min) / bins;
+
+        //Nullptr exception
+        final List<Double> results = new ArrayList<>(bins);
+        // List initialization
+
+        for (double value : samples) {
+            //Shows in which percent of the histogram length the sample is
+            double percentage = (value - min) / max;
+            int index = (int) floor(percentage * bins);
+            // In case when index is equal number of bins, (it can exceed the list size)
+            index = min(index, bins - 1);
+            double freq = results.get(index);
+            results.add(index, freq + 1);
+        }
+
+        return results;
+    }
 }
