1e46d87caf3afd9f6e9cf2c9a8c86f831b6f26ea
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-03-20 20:40:51 +0100

    method declarations

---
+++ model/src/main/java/cps/model/Math.java
@@ -0,0 +1,25 @@
+package cps.model;
+
+public class Math {
+
+    //TODO: Find proper english names
+    public static double averageValue() {
+        throw new UnsupportedOperationException("not implemented yet.");
+    }
+
+    public static double averageAbsoluteValue() {
+        throw new UnsupportedOperationException("not implemented yet.");
+    }
+
+    public static double averagePower() {
+        throw new UnsupportedOperationException("not implemented yet.");
+    }
+
+    public static double variance() {
+        throw new UnsupportedOperationException("not implemented yet.");
+    }
+
+    public static double effectivePower() {
+        throw new UnsupportedOperationException("not implemented yet.");
+    }
+}
