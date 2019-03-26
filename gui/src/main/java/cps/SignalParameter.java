74725d4cb8986a91ae7c129f4abffe1e58894290
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-03-26 22:47:20 +0100

    next sketch of SignalParameter

---
+++ gui/src/main/java/cps/SignalParameter.java
@@ -0,0 +1,32 @@
+package cps;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+
+public class SignalParameter extends HBox {
+
+    @FXML
+    private AnchorPane myTestButton;
+
+    public SignalParameter() {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+
+                getClass().getResource("SignalParameter.fxml"));
+
+
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+}
