08f52f886964732fecbbab299e4ba9396801269d
Jakub Mroczek <jakub.mroczek@tomtom.com>
2019-04-16 23:59:11 +0200

    sketch of foh interpolation

--- model/src/main/java/cps/conversion/Reconstructor.java
+++ model/src/main/java/cps/conversion/Reconstructor.java
@@ -13,6 +13,33 @@ import static java.lang.Math.sin;
 
 public class Reconstructor {
 
+    public static Signal firstHoldInterpolation(final Signal signal, Duration samplingPeriodInNs) {
+        //TODO: Allocatie proper number of memory to make it faster
+        List<Double> samples = new ArrayList<>();
+
+        Duration elapsedTime = Duration.ZERO;
+        final Duration duration = signal.getDurationInNs();
+
+        int previous = 0;
+
+        while (elapsedTime.compareTo(duration) < 0) {
+            //TODO: array out of bounds
+            if (elapsedTime.compareTo(signal.getSamplingPeriod().multipliedBy((previous + 1))) > 0) {
+                previous++;
+            }
+
+            double tangenns = (signal.getSamples().get(previous + 1) - signal.getSamples().get(previous)) / (signal.getSamplingPeriod().toNanos());
+            double b = signal.getSamples().get(previous) - tangenns * signal.getSamplingPeriod().multipliedBy(previous).toNanos();
+
+            double interpolatedValue = b * elapsedTime.toNanos() + b;
+
+            samples.add(interpolatedValue);
+            elapsedTime.plus(samplingPeriodInNs);
+        }
+
+        return new Signal(signal.getType(), signal.getDurationInNs(), samplingPeriodInNs, samples);
+    }
+
     public Signal reconstruct(Signal signal, Duration reconstructionFrequency, int maxProbes) {
         Duration elapsedTime = Duration.ZERO;
         Duration duration = signal.getDurationInNs();
