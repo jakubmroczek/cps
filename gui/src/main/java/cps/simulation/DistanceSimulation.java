1878c25bae5097a9f321d57189270b423e8dca6d
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-05-19 14:25:32 +0200

    implemtned method for getting current tiem unit from the scene

--- gui/src/main/java/cps/simulation/DistanceSimulation.java
+++ gui/src/main/java/cps/simulation/DistanceSimulation.java
@@ -2,16 +2,20 @@ package cps.simulation;
 
 import cps.model.FunctionFactory;
 import cps.model.SignalArgs;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 
 import cps.model.Signal;
-import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import static java.lang.Math.min;
+import static org.apache.commons.lang.time.DateUtils.MILLIS_IN_SECOND;
 
 public class DistanceSimulation {
 
@@ -19,20 +23,15 @@ public class DistanceSimulation {
     private LineChart<Number, Number> transmittedSignalChart, receivedSignalChart;
 
     @FXML
-    private Button startSimulationButton;
+    private TextField timeUnitTextField;
 
-    @FXML
-    public  void start() {
-        var args = SignalArgs.builder().amplitude(1).initialTimeInNs(0).periodInNs(500_000_000).initialTimeInNs(0).build();
-        var sineFunction = FunctionFactory.createFunction(FunctionFactory.SINUSOIDAL, args);
-
-        Signal signal = Signal.createContinousSignal(sineFunction, Duration.ofMillis(1000), Duration.ofMillis(1));
-
-        plot(signal, transmittedSignalChart);
-        plot(signal, receivedSignalChart);
+    private Duration getTimeUnit() {
+        int MILLIS_TO_SECONDS = 1000;
+        var text = timeUnitTextField.getText();
+        //TODO: Exception handling
+        return Duration.ofMillis((long)(Double.valueOf(text) * MILLIS_TO_SECONDS));
     }
 
-    //TODO: It is a common feature
     private void plot(Signal signal, LineChart<Number, Number> chart) {
         XYChart.Series series = new XYChart.Series();
 
@@ -54,5 +53,29 @@ public class DistanceSimulation {
         }
 
         chart.getData().add(series);
+    }
+
+    private void startTransmittingSignal(Function<Double, Double> function) {
+//        Duration timeUnit = getTimeUnit();
+//        Task task = null;
+//        transmittedSignalChart.dataProperty().bind(task.valueProperty());
+//        Thread t = new Thread(() -> {
+//            function.apply()
+//        });
+//        t.start();
+    }
+
+    private void stopTransmittingSignal() {
+    }
+
+    @FXML
+    public  void start() {
+        var args = SignalArgs.builder().amplitude(1).initialTimeInNs(0).periodInNs(500_000_000).initialTimeInNs(0).build();
+        var sineFunction = FunctionFactory.createFunction(FunctionFactory.SINUSOIDAL, args);
+
+        Signal signal = Signal.createContinousSignal(sineFunction, Duration.ofMillis(1000), Duration.ofMillis(1));
+
+        plot(signal, transmittedSignalChart);
+        plot(signal, receivedSignalChart);
     }
 }
