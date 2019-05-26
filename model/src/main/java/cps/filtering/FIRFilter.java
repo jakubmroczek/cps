476b49d2071cffdd7f6336fc7b38d1e41b970bf3
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-05-26 21:49:56 +0200

    finished refactoring

--- model/src/main/java/cps/filtering/FIRFilter.java
+++ model/src/main/java/cps/filtering/FIRFilter.java
@@ -14,28 +14,14 @@ public abstract class FIRFilter {
 
     // TODO: The last three arguments are not elegant, maybe a DTO should be introduced
     public List<Signal> filter(final Signal signal, final int M, final double frequency, final WindowFunction windowFunction) {
+        Signal filterImpulseResponse = createFilterImpulseResponse(signal, M, frequency, windowFunction);
+        Signal filteredSignal = filter(signal, filterImpulseResponse, M);
 
+        ArrayList<Signal> signals = new ArrayList<>();
+        signals.add(filterImpulseResponse);
+        signals.add(filteredSignal);
 
-        Signal filterH = new Signal(Signal.Type.DISCRETE, signal.getSamplingPeriod().dividedBy(M), signal.getSamplingPeriod(),  newValues);
-        Signal convolution = Filters.convolute(filterH, signal);
-
-        List<Double> newConvolutionValues = new ArrayList<>();
-
-        var size = convolution.getSamples().size() - M + 1;
-
-        for (int i = 0; i < size; i++) {
-            newConvolutionValues.add(convolution.getSamples().get(i + (M - 1) / 2));
-        }
-
-        ArrayList<Signal> res = new ArrayList<>();
-        res.add(filterH);
-
-        var duration  = signal.getDurationInNs().minus(convolution.getSamplingPeriod().multipliedBy(M - 1));
-        var filteredSignal = new Signal(signal.getType(), duration, signal.getSamplingPeriod(), newConvolutionValues);
-
-        res.add(filteredSignal);
-
-        return res;
+        return signals;
     }
 
     private Signal createFilterImpulseResponse(final Signal signal, final int M, final double frequency, final WindowFunction windowFunction) {
@@ -44,7 +30,7 @@ public abstract class FIRFilter {
 
         List<Double> newValues = new ArrayList<>();
         for (int i = 0; i < M; i++) {
-            double newSample = 0.0;
+            double newSample;
 
             if (i == (M - 1) / 2) {
                 newSample = 2.0 / K;
@@ -58,6 +44,22 @@ public abstract class FIRFilter {
         }
 
         return new Signal(Signal.Type.DISCRETE, signal.getSamplingPeriod().dividedBy(M), signal.getSamplingPeriod(),  newValues);
+    }
+
+    //TODO: Can be M transpored by a filterImpulseResponse
+    private Signal filter(final Signal signal, final Signal filterImpulseResponse, final int M) {
+        Signal convolution = Filters.convolute(filterImpulseResponse, signal);
+
+        List<Double> newConvolutionValues = new ArrayList<>();
+
+        var size = convolution.getSamples().size() - M + 1;
+
+        for (int i = 0; i < size; i++) {
+            newConvolutionValues.add(convolution.getSamples().get(i + (M - 1) / 2));
+        }
+
+        var duration  = signal.getDurationInNs().minus(convolution.getSamplingPeriod().multipliedBy(M - 1));
+        return new Signal(signal.getType(), duration, signal.getSamplingPeriod(), newConvolutionValues);
     }
 
     protected abstract double modulate(int index);
